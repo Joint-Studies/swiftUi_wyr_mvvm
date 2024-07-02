f3932796f91887ef1a193464bdf6572a8ad53c4d
Rodrigo Silva <rodrigo.amancio@youse.com.br>
2024-07-01 22:15:30 -0300

    Initial Commit

---
+++ wyr_app/wyr_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  wyr_appApp.swift
+//  wyr_app
+//
+//  Created by Rodrigo Silva on 01/07/24.
+//
+
+import SwiftUI
+
+@main
+struct wyr_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
